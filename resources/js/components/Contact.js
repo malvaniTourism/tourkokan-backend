531c84e0cf89c90c71cd35767c433ed6c63cb5f7
Pranav <kamblepranav460@gmail.com>
2024-06-02 12:28:31 +0530

    debug api call and env

--- resources/js/components/Contact.js
+++ resources/js/components/Contact.js
@@ -15,6 +15,7 @@ const Contact = () => {
         e.preventDefault();
 
         try {
+            console.log(appUrl);
             const response = await fetch(`${appUrl}/api/v2/addGuestQuery`, {
                 method: 'POST',
                 headers: {
