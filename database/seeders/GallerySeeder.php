75dbd6b300d10b97bc10969f59218e7c7b8be5e3
Pranav <kamblepranav460@gmail.com>
2024-10-20 01:16:03 +0530

    static ids from script removed and make it dynamic

--- database/seeders/GallerySeeder.php
+++ database/seeders/GallerySeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\City;
 use App\Models\Gallery;
 use App\Models\Site;
+use Exception;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Log;
@@ -21,8 +22,21 @@ class GallerySeeder extends Seeder
     public function run()
     {
         $cities = Site::whereHas('categories', function ($query) {
-            $query->where('id', 3);
+            $query->where('name', 'city');
         })->get();
+
+        $parent_site = Site::where('name', 'Sindhudurg')->first();
+
+        if ($parent_site) {
+            $parent_id = $parent_site->id;
+            // Proceed with your logic using $parent_id
+        } else {
+            // Log an error message
+            Log::error('Site with name "Sindhudurg" not found. Seeder terminated.');
+
+            // Terminate the seeder by throwing an exception
+            throw new Exception('Site with name "Sindhudurg" not found. Seeder terminated.');
+        }
 
         foreach ($cities as $value) {
             $data = getData($value->id, 'Site');
@@ -55,8 +69,8 @@ class GallerySeeder extends Seeder
                 logger([$subSiteName, $subSite]);
                 $site = Site::where("name", $subSiteName)
                     ->where("parent_id", $value->id)
-                    ->orWhere(function ($query) use ($subSiteName) {
-                        $query->where("parent_id", 1)
+                    ->orWhere(function ($query) use ($subSiteName, $parent_id) {
+                        $query->where("parent_id", $parent_id)
                             ->where("name", $subSiteName);
                     })
                     ->first();
@@ -91,7 +105,6 @@ class GallerySeeder extends Seeder
                 }
 
                 $path = Storage::url($destinationFilePath);
-
 
                 Gallery::create([
                     'title' => $title,
